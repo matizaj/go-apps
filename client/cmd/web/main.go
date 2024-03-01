12baf3ad05b30ebcce5c10fa60c59f936c6dbc85
Mateusz <matzaj101@gmail.com>
2024-03-01 18:28:29 +0100

    add templates

---
+++ client/cmd/web/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "test.page.gohtml")
+	})
+
+	fmt.Println("Starting front end service on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func render(w http.ResponseWriter, t string) {
+
+	partials := []string{
+		"./templates/base.layout.gohtml",
+		"./templates/header.partial.gohtml",
+		"./templates/footer.partial.gohtml",
+	}
+
+	var templateSlice []string
+	pwd, _ := os.Getwd()
+	fmt.Println(pwd)
+	templateSlice = append(templateSlice, fmt.Sprintf("./templates/%s", t))
+	fmt.Println(templateSlice)
+	for _, x := range partials {
+		templateSlice = append(templateSlice, x)
+	}
+
+	tmpl, err := template.ParseFiles(templateSlice...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}

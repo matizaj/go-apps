3f5bbfba15b429ca46f4c5fad662e8d99cfa9b4b
Mateusz <matzaj101@gmail.com>
2024-03-02 16:07:50 +0100

    add brokeer service

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+	log.Printf("Starting broker service on port %s\n", webPort)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		fmt.Println("cant strat broker sevice")
+		log.Panic(err)
+	}
+}
---
+++ broker-service/cmd/api/routes.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"net/http"
+)
+
+func (app Config) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// who is allowed tto connect
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://*", "https://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSFR-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	mux.Post("/", app.Broker)
+	return mux
+}

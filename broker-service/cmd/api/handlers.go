a546a20be5517182ced8e9498def1e7b3cb69f1a
Mateusz <matzaj101@gmail.com>
2024-03-03 21:33:19 +0100

    add helper function to handle r&w json

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,ommitempty"`
-}
-
-func (app Config) Broker(w http.ResponseWriter, r *http.Request) {
+func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
 		Error:   false,
 		Message: "hit the broker",
 	}
-
-	out, _ := json.MarshalIndent(payload, "", "\t")
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
+	_ = app.writeJson(w, http.StatusOK, payload, nil)
 }
--- broker-service/cmd/api/routes.go
+++ broker-service/cmd/api/routes.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-func (app Config) routes() http.Handler {
+func (app *Config) routes() http.Handler {
 	mux := chi.NewRouter()
 
 	// who is allowed tto connect

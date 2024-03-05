7e5270c5f4009d0db6e34bcfe022bd038c9ecd5c
Mateusz <matzaj101@gmail.com>
2024-03-05 22:49:03 +0100

    why it not work

---
+++ project/db-data/scripts/init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE inflation_data (RegionalMember TEXT,
+                             Year INT,
+                             Inflation DECIMAL,
+                             Unit_of_Measurement TEXT,
+                             Subregion TEXT,Country_Code TEXT);

59cda0d59b22691b85ad46c1c0355fb42b556429
Mateusz <matzaj101@gmail.com>
2024-03-12 21:05:07 +0100

    add http server

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -1,94 +1,30 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
+	"html/template"
 	"log"
-	"net"
-	"strings"
+	"net/http"
 )
 
-func main() {
-	log.Println("hello mail service!")
-	li, err := net.Listen("tcp", ":5050")
+type hotdog int
+
+func (m hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	body := req.Body
 	if err != nil {
-		log.Panic(err)
+		log.Fatalln(err)
 	}
-	defer li.Close()
 
-	for {
-		conn, err := li.Accept()
-		if err != nil {
-			log.Println(err)
-		}
-		go handle(conn)
-		//io.WriteString(conn, "\n hello from rcp server\n")
-		//fmt.Fprintln(conn, "how is your day?")
-		//fmt.Fprintf(conn, "%v", "Well I hope!")
-		//conn.Close()
-	}
+	tpl.ExecuteTemplate(w, "index.gohtml", req.Form)
 }
 
-func handle(conn net.Conn) {
-	defer conn.Close()
-	request(conn)
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("./cmd/api/index.gohtml"))
 }
 
-func request(conn net.Conn) {
-	i := 0
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		ln := scanner.Text()
-		fmt.Println(ln)
-		if i == 0 {
-			mux(conn, ln)
-		}
-		if ln == "" {
-			break
-		}
-		i++
-	}
-}
-
-func mux(conn net.Conn, ln string) {
-	m := strings.Fields(ln)[0]
-	u := strings.Fields(ln)[1]
-	fmt.Println("*** METHOD: ", m)
-	fmt.Println("*** URI: ", u)
-	if m == "GET" && u == "/" {
-		index(conn)
-	}
-	if m == "GET" && u == "/about" {
-		about(conn)
-	}
-}
-
-func index(conn net.Conn) {
-	body := `<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title></title></head><body>
-	<strong>INDEX</strong><br>
-	<a href="/">index</a><br>
-	<a href="/about">about</a><br>
-	<a href="/contact">contact</a><br>
-	<a href="/apply">apply</a><br>
-	</body></html>`
-	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
-	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
-	fmt.Fprint(conn, "Content-Type: text/html\r\n")
-	fmt.Fprint(conn, "\r\n")
-	fmt.Fprint(conn, body)
-}
-func about(conn net.Conn) {
-	body := `<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title></title></head><body>
-	<strong>ABOUT</strong><br>
-	<a href="/">index</a><br>
-	<a href="/about">about</a><br>
-	<a href="/contact">contact</a><br>
-	<a href="/apply">apply</a><br>
-	</body></html>`
-
-	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
-	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
-	fmt.Fprint(conn, "Content-Type: text/html\r\n")
-	fmt.Fprint(conn, "\r\n")
-	fmt.Fprint(conn, body)
+func main() {
+	var d hotdog
+	http.ListenAndServe(":5050", d)
 }

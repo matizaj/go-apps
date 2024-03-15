6982e8df0fb8a582d5ab7c2aeae636aeb03db751
Mateusz <matzaj101@gmail.com>
2024-03-15 09:54:19 +0100

    read file from request

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -30,13 +31,63 @@ func matt(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./cmd/api/matt.jpg")
 }
 
+func query(w http.ResponseWriter, r *http.Request) {
+	req := r.FormValue("d")
+	io.WriteString(w, req)
+}
+
+func post(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `
+	<form method="get">
+		<input type="text" name="q">
+		<input type="submit">
+	</form>
+`)
+}
+func file(w http.ResponseWriter, r *http.Request) {
+	var s string
+	fmt.Println("METHOD: ", r.Method)
+	if r.Method == http.MethodPost {
+		// open
+		f, h, err := r.FormFile("q")
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		defer f.Close()
+
+		//fyi
+		fmt.Println("file: ", f, "\nheader: ", h)
+
+		bs, err := io.ReadAll(f)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		s = string(bs)
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `
+	<form method="post" enctype="multipart/form-data">
+		<input type="file" name="q">
+		<input type="submit">
+	</form>
+`+s)
+}
+
 func main() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
 	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
 	//http.HandleFunc("/", dog)
-	http.HandleFunc("/matt", matt)
+	//http.HandleFunc("/matt", matt)
+	http.HandleFunc("/query", query)
+	http.HandleFunc("/post", post)
+	http.HandleFunc("/file", file)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
 	err := http.ListenAndServe(":5050", nil)
 	if err != nil {
-		panic("server cant start")
+		panic(err)
 	}
 }

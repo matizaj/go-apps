51468b763aa12cfe0fab34a9934cd56abb4dd4a5
Mateusz <matzaj101@gmail.com>
2024-03-18 10:32:26 +0100

    basic types

--- hello/cmd/main.go
+++ hello/cmd/main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
-	hello "TheGoPath"
 	"fmt"
 	"os"
 )
 
 func main() {
-	fmt.Println(hello.SayHello(os.Args[1:]))
+	var sum float64
+	var n int
+
+	for {
+		var val float64
+		if _, err := fmt.Fscanln(os.Stdin, &val); err != nil {
+			break
+		}
+		sum += val
+		n++
+	}
+
+	if n == 0 {
+		fmt.Fprintln(os.Stderr, "no values")
+		os.Exit(-1)
+	}
+
+	fmt.Println(" The average is: ", sum/float64(n))
 
 }

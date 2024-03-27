955e655492e8843f9b98d6120f3135fab142f488
Mateusz <matzaj101@gmail.com>
2024-03-27 08:26:49 +0100

    add listener service to infra

---
+++ listener-service/go.mod
@@ -0,0 +1,5 @@
+module github.com/matizaj/go-app/listener
+
+go 1.22.0
+
+require github.com/rabbitmq/amqp091-go v1.9.0 // indirect
---
+++ listener-service/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// connect rabbitmq
+	// start listening for messages
+	// create consumer - consume massages from queue
+	// watch queue - consume events
+
+}

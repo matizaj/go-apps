f43a89d405664e03279ff7a78d930281604a83ae
Mateusz <matzaj101@gmail.com>
2024-03-27 14:48:50 +0100

    add rabbmitmq

--- listener-service/main.go
+++ listener-service/main.go
@@ -1,9 +1,54 @@
 package main
 
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+	"math"
+	"os"
+	"time"
+)
+
 func main() {
 	// connect rabbitmq
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println("cant conect rabbitmq ", err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
+	log.Println("Connected to RabbitMQ")
 	// start listening for messages
 	// create consumer - consume massages from queue
 	// watch queue - consume events
 
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = time.Second * 1
+	var connection *amqp.Connection
+
+	// dont continue until rabbit is ready
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		if err != nil {
+			fmt.Println("rabbitmq not ready")
+			counts++
+		} else {
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("...backing off...")
+		time.Sleep(backOff)
+
+	}
+	return connection, nil
 }

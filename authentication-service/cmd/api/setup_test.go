9e1f49887db46d83de9f396341ab446c9d2fddb6
Mateusz <matzaj101@gmail.com>
2024-06-01 12:13:24 +0200

    setup tests

--- authentication-service/cmd/api/setup_test.go
+++ authentication-service/cmd/api/setup_test.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"github.com/matizaj/go-app/authentication-service/data"
 	"os"
 	"testing"
 )
 
-func TestMain(m *testing.M) {
+var testApp Config
 
+func TestMain(m *testing.M) {
+	repo := data.NewPostgresTestRepo(nil)
+	testApp.Repo = repo
 	os.Exit(m.Run())
 }
